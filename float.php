f3ae6ab1265dcefbd4b9f640b1cc99ff00ad2dc4
melpenyogi <melpenyogi2019@gmail.com>
2023-09-19 17:09:45 +0700

    Tipe data Float

---
+++ float.php
@@ -0,0 +1,32 @@
+<?php
+$x = 10.6;
+$y = 77.7;
+
+$tambah = $x + $y;
+
+var_dump($x);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipe Data Float</title>
+</head>
+
+<body>
+
+    <p>Tipe Data float (bilangan pecahan)</p>
+    <p><?= $x ?></p>
+    <p><?= $y ?></p>
+    <p><?= $x ?> + <?= $y ?> = <?= $tambah ?></p>
+
+
+</body>
+
+</html>
