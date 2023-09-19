9ba3a5d1e775cc9d4605006e3add19009c3ddba0
melpenyogi <melpenyogi2019@gmail.com>
2023-09-19 16:36:48 +0700

    string

---
+++ string.php
@@ -0,0 +1,24 @@
+<?php
+// TIPE DATANG STRING
+$title = "KELAS PHP SACODE";
+$nama_depan = 'Melpen';
+$nama_belakang = 'Yogi';
+$alamat = 'Waena';
+$hoby = "editor";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TIPE DATA STRING</title>
+</head>
+
+<body>
+    <h2> <?= $title ?> </h2>
+    <p> Nama saya <?= $nama_depan  . ' ' . $nama_belakang ?> </p>
+</body>
+
+</html>
